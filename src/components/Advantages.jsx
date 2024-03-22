72d69e1528281ac8b7491927910066bcdb5859c4
Grigore <voytov@mssc.su>
2024-03-22 17:47:08 +0700

    Update block advantages

--- src/components/Advantages.jsx
+++ src/components/Advantages.jsx
@@ -1,10 +1,11 @@
 import ADVANTAGES_LIST from './Data/ADVANTAGES_LIST'
 import TitleSection from '../UI/Title'
+import styles from '../styles/advantages.module.scss'
 
 function Advantages() {
   return (
-    <section className="page-main__advantages main-advantages">
-      <div className="wrapper animation-container">
+    <section className={styles.advantages}>
+      <div className={styles.wrapper}>
         <TitleSection className="main-advantages__title">Преимущества</TitleSection>
 
         <ul className="main-advantages__list">
