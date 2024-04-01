ec058f7787eea2854864adcde0521caa5e4d7d28
Grigore <voytov@mssc.su>
2024-04-01 16:29:43 +0700

    Фикс интерактивной карты (включение пока не работает)

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/contacts.module.scss'
 function Contacts() {
   return (
     <address className={styles.contacts}>
-      <button className={styles.item} onClick="">г. Красноярск, ул. 60 лет Октября д. 90, оф. 2-16</button>
+      <button className={styles.item}>г. Красноярск, ул. 60 лет Октября д. 90, оф. 2-16</button>
       <a className={styles.item} href="mailto:smtrade@mssc.su">smtrade@mssc.su</a>
       <a className={styles.item} href="tel:+739123447073">+7 (391) 234-70-73</a>
     </address>
