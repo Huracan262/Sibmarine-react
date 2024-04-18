959f023a1529ff2e21130a680620a934e962c3b1
Grigore <voytov@mssc.su>
2024-04-18 14:44:40 +0700

    AboutTeam | Создание блока | Добавлен черновой вариант блока нашей команды

--- src/components/AboutTeam.jsx
+++ src/components/AboutTeam.jsx
@@ -1,8 +1,25 @@
+import MEMBERS_LIST from './Data/MEMBERS_LIST'
+import undefined from '../img/team/undefined.png'
+import styles from '../styles/AboutTeam.module.scss'
+
 function AboutTeam() {
   return (
-    <div>
-      Гриша, Саша, Миша, Костя
-    </div>
+    <ul className={styles.list}>
+      {MEMBERS_LIST.map(({ id, position, name, photoUrl }) => {
+        return (
+          <li className={styles.item} key={id}>
+            <figure className={styles.figure}>
+              <figcaption className={styles.figcaption}>
+                <h3 className={styles.name}>{name}</h3>
+                <p className={styles.description}>{position}</p>
+              </figcaption>
+
+              <img className={styles.img} src={!!photoUrl ? photoUrl : undefined} alt={name} />
+            </figure>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
---
+++ src/components/Data/MEMBERS_LIST.js
@@ -0,0 +1,34 @@
+const MEMBERS_LIST = [
+  {
+    id: 1,
+    name: 'Макаревич Александр Сергеевич',
+    position: 'Директор',
+    photoUrl: ''
+  },
+  {
+    id: 2,
+    name: 'Белов Анатолий Иванович',
+    position: 'Заместитель директора',
+    photoUrl: ''
+  },
+  {
+    id: 3,
+    name: 'Русов Александр',
+    position: 'Менеджер по работе с ключевыми клиентами',
+    photoUrl: ''
+  },
+  {
+    id: 4,
+    name: 'Войтов Григорий',
+    position: 'FullStack разработчик',
+    photoUrl: ''
+  },
+  {
+    id: 5,
+    name: 'Александр Филатов',
+    position: 'Менеджер тендерного отдела',
+    photoUrl: ''
+  }
+]
+
+export default MEMBERS_LIST
