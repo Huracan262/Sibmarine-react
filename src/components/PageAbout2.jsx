8b536811af5ca4a192985db802669fe2da2fdc76
Grigore <voytov@mssc.su>
2024-05-06 17:29:57 +0700

    PageAbout | Добавление элементов | • Сделана верстка нашей команды. • Сделана верстка блока "Напишите нам"

--- src/components/PageAbout2.jsx
+++ src/components/PageAbout2.jsx
@@ -3,11 +3,15 @@ import Marquee from 'react-fast-marquee'
 import Breadcrumb from '../components/Breadcrumb'
 import Title from '../UI/Title'
 import styles from '../styles/PageAbout2.module.scss'
+import MEMBERS_LIST from './Data/MEMBERS_LIST'
+import undefined from '../img/team/undefined.png'
+import Button from '../UI/Button'
+import SOCIALS_LIST from './Data/SOCIALS_LIST'
 
 function PageAbout() {
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+  // useEffect(() => {
+  //   window.scrollTo(0, 0)
+  // }, [])
 
   return (
     <main className={styles.pageAbout}>
@@ -142,20 +146,72 @@ function PageAbout() {
         </div>
       </section>
 
-      <section>
+      <section className={styles.team}>
         <div className={styles.wrapper}>
           <Title>Наши сотрудники</Title>
 
-          <ul>
-            <li>
+          <ul className={styles.teamList}>
+            {MEMBERS_LIST.map(({ id, position, name, photoUrl }) => {
+              return (
+                <li className={styles.teamItem} key={id}>
+                  <figure className={styles.teamFigure}>
+                    <figcaption className={styles.teamFigcaption}>
+                      <h3 className={styles.teamName}>{name}</h3>
+                      <p className={styles.teamDescription}>{position}</p>
+                    </figcaption>
 
-            </li>
+                    <img className={styles.teamImg} src={!!photoUrl ? photoUrl : undefined} alt={name} />
+                  </figure>
+                </li>
+              )
+            })}
           </ul>
         </div>
       </section>
 
-      <article>
-        <h2>Напишите нам</h2>
+      <article className={styles.write}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.writeTitle}>Напишите нам</h2>
+
+          <div className={styles.writeContainer}>
+            <form className={styles.writeForm}>
+              <div className={styles.writeBlock}>
+                <label className={'visually-hidden'} htmlFor="name">Ваше имя:</label>
+                <input className={styles.input} type="text" name="name" id="name" maxLength="50" required
+                       placeholder="Ваше имя"/>
+              </div>
+
+              <div className={styles.writeBlock}>
+                <label className={'visually-hidden'} htmlFor="contacts">Контакты:</label>
+                <input className={styles.input} type="text" name="contacts" id="contacts" maxLength="50" required
+                       placeholder="Email / телефон"/>
+              </div>
+
+              <div className={styles.writeBlock}>
+                <label className={'visually-hidden'} htmlFor="text">Заявка:</label>
+                <input className={styles.input} type="text" name="text" id="text" maxLength="200" required placeholder="Введите ваш запрос"/>
+              </div>
+
+              <button className={styles.writeButton} type="submit">Связаться с нами</button>
+
+              <ul className={styles.writeList}>
+                {SOCIALS_LIST.map(item => (
+                  <li className={styles.writeItem} key={item.name}>
+                    <a className={styles.writeLink} href={item.url}>
+                      {item.logo}
+                      <span className="visually-hidden">{item.name}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </form>
+
+            <p className={styles.writePicture}>
+              Здесь будет картинка
+              <img className={styles.writeImg} src="" alt="" />
+            </p>
+          </div>
+        </div>
       </article>
     </main>
   )
